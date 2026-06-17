e6c07e444b41aa7f16712096538a21bc31dbe529
Aayush Gour <aayushgour@Aayushs-MacBook-Air.local>
2026-06-17 11:08:12 +0530

    removed hash router, replaced with browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './store/settingsStore' // apply saved theme on load
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/devhub">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
